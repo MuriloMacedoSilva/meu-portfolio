7afda4bf8f7611737c0dc6bfaf21fe69a3e42aed
MuriloMacedoSilva <152343609+MuriloMacedoSilva@users.noreply.github.com>
2025-07-22 12:58:56 -0300

    atualizando portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import background from "../src/assets/background.svg";
 import pessoalMurilo from "../src/assets/pessoalMurilo.svg";
 import eclipse1 from "../src/assets/Ellipse1.svg";
 import "../src/App.css";
-import { MoveDown } from 'lucide-react';
-import { Menu } from 'lucide-react';
+import { MoveDown } from "lucide-react";
+import { Menu } from "lucide-react";
 
 function App() {
   background;
@@ -13,14 +13,14 @@ function App() {
     <html className="custom-cursor">
       <div className="bg-[url('../src/assets/background.svg')] bg-cover bg-center w-screen">
         <div className="-bg-linear-210 from-black/90 from-5% to-100% xl:from-25% via-white/90 to-black/80 w-screen justify-center">
-        <header className=" w-full items-center justify-start px-[60px] py-[40px]">
-          <div className="text-white hover:bg-yellow-400 hover:text-yellow-400 rounded-full w-[40px] h-[40px] py-1 border-white items-center justify-center flex transition duration-250 ease-in-out">
-            <Menu />
-          </div>
-        </header>
+          <header className=" w-full items-center justify-start px-[60px] py-[40px]">
+            <div className="text-white hover:bg-yellow-400 hover:text-yellow-400 rounded-full w-[40px] h-[40px] py-1 border-white items-center justify-center flex transition duration-250 ease-in-out">
+              <Menu />
+            </div>
+          </header>
           <main className=" w-screen flex flex-col items-center">
             <section className="w-[80%] flex flex-col gap-3 items-center sm:gap-9 md:flex md:flex-row-reverse md:justify-between xl:w-[90%] xl:px-[70px]">
-              <div className="cursor-pointer w-[75%] h-[270px] rounded-full overflow-hidden border-[4px] border-white/90 shadow-white shadow-xl/30 flex items-center sm:w-[400px] sm:h-[400px] md:w-[800px] md:h-[400px] lg:w-[450px] lg:h-[450px] xl:w-[450px] xl:h-[450px]">
+              <div className="bg-white cursor-pointer w-[75%] h-[270px] rounded-full overflow-hidden border-[4px] border-white/90 shadow-white shadow-xl/30 flex items-center justify-center sm:w-[400px] sm:h-[400px] md:w-[800px] md:h-[400px] lg:w-[450px] lg:h-[450px] xl:w-[450px] xl:h-[450px]">
                 <img
                   className=" w-full"
                   src={pessoalMurilo}
@@ -58,9 +58,11 @@ function App() {
                 <MoveDown />
               </div>
             </section>
-            <section className=" bg-gray-300/90 w-full px-[50px] flex items-center justify-center pt-13">
+            <section className=" bg-gray-100/90 w-full px-[50px] flex items-center justify-center pt-13">
               <div className="w-full flex flex-col items-center justify-center pt-[40px] pb-[100px] gap-11">
-                <h2 className="text-gray-900 font-light text-shadow-sm text-shadow-white/30 text-[50px]">Sobre</h2>
+                <h2 className="text-gray-900 font-light text-shadow-sm text-shadow-white/30 text-[50px]">
+                  Sobre
+                </h2>
                 <p className="text-black font-light text-[18px] text-center xl:w-[700px] xl:text-[19px]">
                   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut
                   iusto delectus doloribus est enim quas saepe, minima totam sit
@@ -69,11 +71,19 @@ function App() {
                   consectetur adipisicing elit. Sint obcaecati, ipsam enim
                   repudiandae facilis aut perspiciatis ea quidem minima numquam
                   voluptatem commodi sed ut exercitationem. Suscipit temporibus
-                  beatae explicabo eligendi? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem quo libero placeat nobis repellat obcaecati odit, cupiditate labore architecto neque ratione maxime laboriosam magnam delectus voluptatum reiciendis et vero? Tempore?Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, eveniet tempora! Aliquid commodi dolore itaque perspiciatis. Ad facilis nesciunt sit laboriosam pariatur distinctio ipsam dignissimos, delectus, natus ullam, maxime dolores.
+                  beatae explicabo eligendi? Lorem ipsum, dolor sit amet
+                  consectetur adipisicing elit. Exercitationem quo libero
+                  placeat nobis repellat obcaecati odit, cupiditate labore
+                  architecto neque ratione maxime laboriosam magnam delectus
+                  voluptatum reiciendis et vero? Tempore?Lorem ipsum dolor sit
+                  amet consectetur adipisicing elit. Incidunt, eveniet tempora!
+                  Aliquid commodi dolore itaque perspiciatis. Ad facilis
+                  nesciunt sit laboriosam pariatur distinctio ipsam dignissimos,
+                  delectus, natus ullam, maxime dolores.
                 </p>
               </div>
             </section>
-            <section className=" w-full pb-[50px] px-[70px] -bg-linear-180 from-gray-300/90 flex flex-col items-center gap-17">
+            <section className=" w-full pb-[50px] px-[70px] -bg-linear-180 from-gray-100/90 flex flex-col items-center gap-17">
               <h2 className="text-gray-900 text-shadow-lg text-shadow-white/30 font-light text-[50px]">
                 Habilidades
               </h2>
