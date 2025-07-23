62dc29134703067a256f1612b0b61777f60800fc
MuriloMacedoSilva <152343609+MuriloMacedoSilva@users.noreply.github.com>
2025-07-23 12:20:19 -0300

    contatos do portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -17,20 +17,22 @@ function App() {
     <html className="custom-cursor">
       <div className="bg-[url('../src/assets/background.svg')] bg-cover bg-center w-screen">
         <div className="-bg-linear-210 from-black/90 from-5% to-100% xl:from-25% via-white/90 to-black/80 w-screen justify-center">
-          <header className=" w-full items-center justify-start px-[60px] py-[40px]">
+          <header className=" w-full items-center justify-start px-[84px] py-[40px]">
             <div className="text-white hover:bg-yellow-400 hover:text-yellow-400 rounded-full w-[40px] h-[40px] py-1 border-white items-center justify-center flex transition duration-250 ease-in-out">
               <Menu />
             </div>
           </header>
           <main className=" w-screen flex flex-col items-center">
-            <section className="w-[80%] flex flex-col gap-3 items-center sm:gap-9 md:flex md:flex-row-reverse md:justify-between xl:w-[90%] xl:px-[70px]">
-              <div className="bg-white cursor-pointer w-[75%] h-[270px] rounded-full overflow-hidden border-[4px] border-white/90 shadow-white shadow-xl/30 flex items-center justify-center sm:w-[400px] sm:h-[400px] md:w-[800px] md:h-[400px] lg:w-[450px] lg:h-[450px] xl:w-[450px] xl:h-[450px]">
-                <img
-                  className=" w-full"
-                  src={pessoalMurilo}
-                  alt="foto pessoal"
-                />
-              </div>
+            <section className="w-[86%] flex flex-col gap-3 items-center sm:gap-9 md:flex md:flex-row-reverse md:justify-between">
+              <a href="https://github.com/MuriloMacedoSilva" target="_blank" className="hover:scale-105 transition duration-150">
+                <div className="bg-white cursor-pointer w-[75%] h-[270px] rounded-full overflow-hidden border-[4px] border-white/90 shadow-white shadow-xl/30 flex items-center justify-center sm:w-[400px] sm:h-[400px] md:w-[800px] md:h-[400px] lg:w-[450px] lg:h-[450px] xl:w-[450px] xl:h-[450px]">
+                  <img
+                    className=" w-full"
+                    src={pessoalMurilo}
+                    alt="foto pessoal"
+                  />
+                </div>
+              </a>
 
               <div className="gap-2 flex flex-col md:gap-11 lg:w-[400px] xl:w-[500px]">
                 <div className="w-full pt-7 items-center flex flex-col lg:text-left lg:items-start">
@@ -49,12 +51,23 @@ function App() {
                   </p>
                 </div>
                 <div className=" w-full flex flex-col items-center justify-center gap-2 pt-4 lg:gap-2.5 lg:flex lg:flex-row-reverse xl:w-full xl:justify-start xl:items-start">
-                  <button className="w-full border border-gray-50 text-gray-50 font-medium cursor-pointer rounded-2xl h-[50px] flex items-center justify-center text-[18px] lg:w-[300px] xl:w-[300px] hover:bg-white hover:text-cyan-950 transition duration-280">
-                    Download CV
-                  </button>
-                  <button className="w-full border border-gray-50 font-medium cursor-pointer rounded-2xl h-[50px] flex items-center justify-center bg-white text-cyan-950 text-[18px] lg:w-[300px] xl:w-[300px]">
-                    Contato
-                  </button>
+                  <a
+                    href="https://drive.google.com/file/d/1VKFXExKqfd9QubFzZNgpAqSs-7Bst7_I/view?usp=drive_link"
+                    target="_blank"
+                    className="w-full items-center justify-center flex"
+                  >
+                    <button className="w-full border border-gray-50 text-gray-50 font-medium cursor-pointer rounded-2xl h-[50px] flex items-center justify-center text-[18px] hover:bg-white hover:text-cyan-950 transition duration-280">
+                      Download CV
+                    </button>
+                  </a>
+                  <a
+                    href=""
+                    className="w-full items-center justify-center flex"
+                  >
+                    <button className="w-full border border-gray-50 font-medium cursor-pointer rounded-2xl h-[50px] flex items-center justify-center bg-white text-cyan-950 text-[18px]">
+                      Contato
+                    </button>
+                  </a>
                 </div>
               </div>
             </section>
