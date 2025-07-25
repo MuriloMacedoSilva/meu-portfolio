e2346d1ab195a98fe40b808430c477d228837fb9
MuriloMacedoSilva <152343609+MuriloMacedoSilva@users.noreply.github.com>
2025-07-25 14:11:58 -0300

    portfolio

--- src/components/Sobre.jsx
+++ src/components/Sobre.jsx
@@ -11,7 +11,7 @@ function Sobre() {
       scrollTrigger: {
         trigger: "#Sobre",
         start: "top 80px",
-        end: "bottom 1100px",
+        end: "bottom 1250px",
         scrub: true,
       },
     });
